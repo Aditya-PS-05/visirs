da5e8c5fd7e39c0fcc2b61f681d51675b4fb07c8
Aditya-PS-05 <adityapratapsjnhh7654@gmail.com>
2025-11-13 23:44:40 +0530

    feat: add structs for jsAsset

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,23 @@
+#![deny(clippy::all)]
+
 mod visual_grouping;
+
+use napi_derive::napi;
+use napi::bindgen_prelude::*;
+
+#[napi]
+pub fn plus_100(input: u32) -> u32 {
+    input + 100
+}
+
+#[napi(object)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct JsAsset {
+    pub id: String,
+    pub name: String,
+    pub path: String,
+    pub mime_type: String,
+    pub is_video: bool,
+}
+
+
